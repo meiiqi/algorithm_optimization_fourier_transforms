7215af82eac7a97fb2a2c6d11c7d7bf7585ff8d6
Mei Tang <mei.q.tang@mail.mcgill.ca>
2020-04-26 22:47:42 -0400

    initial implementation of DFT

--- main.c
+++ main.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <complex.h>
 
-#define sample_rate 360 // # samples per iteration
+#define sample_rate 180 // # samples per iteration
 #define PI 3.14159265
 
-float * DFT (float * time_signal)
+float complex complexExp(float x)
 {
-    float * freq_signal = malloc(sizeof(float) * sample_rate);
+    float arg = 2 * PI * x;
+    return cos(arg) + sin(arg) * I;
+}
+
+float complex * DFT (float * time_signal)
+{
+    int s = -1;
+    float complex * freq_signal = malloc(sizeof(float complex) * sample_rate);
+
     for (int i = 0; i < sample_rate; i++)
     {
-        freq_signal[i] = time_signal[i] + 1;
+        for (int j = 0; j < sample_rate; j++)
+        {
+            freq_signal[i] = freq_signal[i] + time_signal[j] * complexExp( (s * i * j) / sample_rate);
+            // printf("Freq signal = %.3f + %.3fi\n", creal(freq_signal[i]), cimag(freq_signal[i]));
+        }
     }
+
     return freq_signal;
 }
 
-void * FFT (void * time_signal)
+float complex * FFT (float * time_signal)
 {
     return NULL;
 }
@@ -31,29 +45,49 @@ void generate_signal (float * signal)
         float t_deg = i * resolution_deg;
 
         signal[i] = sin(t_rad);
-        printf("generating: %f with t = %f\n", sin(t_rad), t_deg);
+        // printf("generating: %f with t = %f\n", sin(t_rad), t_deg);
     }
 }
 
 int main(void)
 {
-    printf("hello!");
+    // printf("hello!\n");
 
     float time_signal[sample_rate];
     generate_signal(time_signal);
 
     // time
-    float * new_signal = DFT(time_signal);
+    float complex * new_signal = DFT(time_signal);
 
     // test
+    // for (int i = 0; i < sample_rate; i++)
+    // {
+    //     printf("[%i]: %f + 1 ==? %f \n", i, time_signal[i], new_signal[i]);
+    // }
+
     for (int i = 0; i < sample_rate; i++)
     {
-        printf("[%i]: %f + 1 ==? %f \n", i, time_signal[i], new_signal[i]);
+        printf("[%d] %.2f + %.2fi\n", i, creal(new_signal[i]), cimag(new_signal[i]));
     }
 
+    // float complex z1 = 1.0 + 3.0 * I;
+    // float complex z2 = 1.0 - 4.0 * I;
+
+    // printf("Starting values: Z1 = %.2f + %.2fi\tZ2 = %.2f %+.2fi\n", 
+    //        creal(z1), 
+    //        cimag(z1), 
+    //        creal(z2), 
+    //        cimag(z2));
+
+    // float complex z3 = ccos(45.0 * PI / 180) + csin(45.0 * PI / 180) * I;
+    // float complex z3 = cos(45.0 * PI / 180) + sin(45.0 * PI / 180) * I;
+
+    // printf("Z3 = %.3f + %.3fi", creal(z3),cimag(z3));
+
+ 
 
     // time
-    FFT(NULL);
+    // FFT(NULL);
     // time
 
     // compare performance
